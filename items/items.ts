ccef92644e485b31083f37576f824e5faedf2351
Cory Chilton <coryhookchilton@gmail.com>
2023-07-24 14:37:39 -0700

    initial commit

---
+++ items/items.ts
@@ -0,0 +1,16 @@
+export const items = [
+  {
+    name: 'Player Safety',
+    price: 30,
+    quantity: 5,
+    description: 'Official CARL hand signal.',
+    imagePath: '/images/playerSafety.jpg'
+  },
+  {
+    name: 'Lightning McDean',
+    price: 80,
+    quantity: 10,
+    description: 'If you wanna play like the man, you first gotta look like the man.',
+    imagePath: '/images/lightningMcDean.jpg'
+  }
+]
