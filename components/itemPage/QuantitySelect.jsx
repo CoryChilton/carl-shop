39626454b7405c0c7cd1c8fea87e5768a9ef3ea7
Cory Chilton <coryhookchilton@gmail.com>
2023-07-26 16:34:20 -0700

    Added quantity button disable at quantity of 1

--- components/itemPage/QuantitySelect.jsx
+++ components/itemPage/QuantitySelect.jsx
@@ -2,21 +2,21 @@ export default function QuantitySelect({onIncrease, onDecrease, quantity}) {
   return (
     <div className="flex w-28 h-12 justify-between items-center border border-gray-200 rounded-sm mb-4">
       <div className="h-full border-r border-gray-200 rounded-r-sm">
-        <QuantityButton onClick={onDecrease}>-</QuantityButton>
+        <QuantityButton onClick={onDecrease} disabled={quantity===1}>-</QuantityButton>
       </div>
       <div className="h-full w-full flex items-center justify-center shadow-inner">
         <h1 className="">{quantity}</h1>
       </div>
       <div className="h-full border-l border-gray-200 rounded-l-sm">
-        <QuantityButton onClick={onIncrease}>+</QuantityButton>
+        <QuantityButton onClick={onIncrease} disabled={false}>+</QuantityButton>
       </div>
     </div>
   )
 }
 
-function QuantityButton({onClick, children}){
+function QuantityButton({onClick, children, disabled}){
   return (
-    <button onClick={onClick} className="bg-gray-100 w-8 flex-none h-full hover:bg-gray-300 ease-in duration-150 active:bg-gray-400">
+    <button onClick={onClick} disabled={disabled} className="bg-gray-100 w-8 flex-none h-full hover:bg-gray-300 ease-in duration-150 active:bg-gray-400 disabled:bg-gray-100 disabled:text-gray-400">
       {children}
     </button>
   )
