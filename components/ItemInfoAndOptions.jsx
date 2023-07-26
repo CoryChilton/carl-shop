ef43943e55305d8c87bfd1c9405f04e2ee4a68d8
Cory Chilton <coryhookchilton@gmail.com>
2023-07-26 13:33:06 -0700

    Added quantity select

--- components/ItemInfoAndOptions.jsx
+++ components/ItemInfoAndOptions.jsx
@@ -1,7 +1,11 @@
+'use client'
 import SizeSelect from './SizeSelect'
 import QuantitySelect from './QuantitySelect'
+import { useState } from 'react';
 
 export default function ItemInfoAndOptions({item}) {
+  const [quantity, setQuantity] = useState(1);
+  let total = item.price * quantity;
 
   return (
     <>
@@ -9,8 +13,10 @@ export default function ItemInfoAndOptions({item}) {
       <h2>{item.description}</h2>
       <SizeSelect />
       <h3>Price: ${item.price}.00</h3>
-      <QuantitySelect />
-      <h3>Total: item.price * quantity</h3>
+
+      <QuantitySelect onIncrease={() => setQuantity(quantity+1)} onDecrease={() => quantity > 0 && setQuantity(quantity-1)} quantity={quantity} />
+
+      <h3>Total: ${total}.00</h3>
       <button className="block">Add to Cart</button>
       <button className="block">Buy Now</button>
     </>
--- components/QuantitySelect.jsx
+++ components/QuantitySelect.jsx
@@ -1,14 +1,9 @@
-'use client'
-import { useState } from 'react';
-
-export default function QuantitySelect() {
-  const [quantity, setQuantity] = useState(0);
-
+export default function QuantitySelect({onIncrease, onDecrease, quantity}) {
   return (
     <>
-      <button onClick={() => quantity > 0 && setQuantity(quantity-1)}>-</button>
+      <button onClick={onDecrease}>-</button>
       <h1>{quantity}</h1>
-      <button onClick={() => setQuantity(quantity+1)}>+</button>
+      <button onClick={onIncrease}>+</button>
     </>
   )
 }
