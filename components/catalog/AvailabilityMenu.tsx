66b4138245a4b7b525f869df572e2263c02afada
Cory Chilton <coryhookchilton@gmail.com>
2023-09-15 13:24:38 -0700

    Fix AvailabilityMenu not vertically centered bug

--- components/catalog/AvailabilityMenu.tsx
+++ components/catalog/AvailabilityMenu.tsx
@@ -13,7 +13,7 @@ export default function AvailabilityMenu({
   const [hide, setHide] = useState(true);
 
   return (
-    <div onAnimationEnd={() => setHide(!hide)} className={`${(hide && !show) && 'hidden'} ${show ? 'animate-fadeInMenu' : 'animate-fadeOutMenu'} border border-slate-400 absolute top-8 -left-1 z-10 bg-white rounded-2xl py-2 px-4 flex fill-mode-forwards`}>
+    <div onAnimationEnd={() => setHide(!hide)} className={`${(hide && !show) && 'hidden'} ${show ? 'animate-fadeInMenu' : 'animate-fadeOutMenu'} border border-slate-400 absolute top-8 -left-1 z-10 bg-white rounded-2xl py-2 px-4 flex items-center fill-mode-forwards`}>
       <input type="checkbox" checked={inStockChecked} onChange={checkInStock} className="inline-block"/>
       <div className="inline-block ml-4 text-slate-600 whitespace-nowrap">
         In Stock
